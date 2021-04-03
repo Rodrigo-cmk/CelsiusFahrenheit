8c2dd1e1fe787e26560eb34fa69b978010b8a86d
Rodrigo Alves <rodrigorac.2003@gmail.com>
2021-04-02 21:07:47 -0300

    Conversor Celsius para Fahrenheit

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace CelsiusFahrenheit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+
+
+
+
+
+            double C;
+            double F;
+            string X;
+
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Conversor Celsius para Fahrenheit");
+            Console.ResetColor();
+            Console.WriteLine("--- Digite um valor em Celsius a ser convertido para Fahrenheit.");
+            Console.WriteLine();
+
+            X = Console.ReadLine();
+            Console.WriteLine();
+
+            C = Convert.ToDouble(X);
+            F = C * 1.8 + 32;
+
+            Console.WriteLine($"Conversão: {F:N2} °F.");
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
